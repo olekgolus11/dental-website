eb079ff2e84c0bf60449c990430ab2a3b53c2696
olasgolas11 <olekgolus02@gmail.com>
2022-08-27 21:26:34 +0200

    add basic responsiveness

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,28 @@
+import NavButton from "./NavButton";
+
 const NavBar = () => {
-    return <div className="navbar-container"></div>;
+    return (
+        <div className="navbar">
+            <div className="navbar-container">
+                <div className="logo-and-burger-container">
+                    <img className="dental-logo" src="/images/stomalogo.jpg" />
+                    <div className="burger" />
+                </div>
+                <div className="buttons-container">
+                    <NavButton>STRONA GŁÓWNA</NavButton>
+                    <NavButton>O NAS</NavButton>
+                    <NavButton>ZAKRES USŁUG</NavButton>
+                    <NavButton>KONTAKT</NavButton>
+                    <NavButton>
+                        <img
+                            className="facebook-logo"
+                            src="/images/facebook.png"
+                        />
+                    </NavButton>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
