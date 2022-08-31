ed3f81a785f30ee6fc325803470490d121f16897
olasgolas11 <olekgolus02@gmail.com>
2022-08-31 20:39:24 +0200

    add mainpage text

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import NavBar from "./components/NavBar/NavBar";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
     return (
         <div>
             <NavBar />
-            <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                finibus mauris ut sem vehicula, eget elementum sapien dignissim.
-                Duis rutrum fringilla orci eget tincidunt. Curabitur congue
-                magna non tortor fringilla lacinia. Integer augue leo, vulputate
-                vitae ligula in, laoreet viverra eros. Proin consequat iaculis
-                pharetra. Fusce ac imperdiet tortor, non scelerisque felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                finibus mauris ut sem vehicula, eget elementum sapien dignissim.
-                Duis rutrum fringilla orci eget tincidunt. Curabitur congue
-                magna non tortor fringilla lacinia. Integer augue leo, vulputate
-                vitae ligula in, laoreet viverra eros. Proin consequat iaculis
-                pharetra. Fusce ac imperdiet tortor, non scelerisque felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                finibus mauris ut sem vehicula, eget elementum sapien dignissim.
-                Duis rutrum fringilla orci eget tincidunt. Curabitur congue
-                magna non tortor fringilla lacinia. Integer augue leo, vulputate
-                vitae ligula in, laoreet viverra eros. Proin consequat iaculis
-                pharetra. Fusce ac imperdiet tortor, non scelerisque felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                finibus mauris ut sem vehicula, eget elementum sapien dignissim.
-                Duis rutrum fringilla orci eget tincidunt. Curabitur congue
-                magna non tortor fringilla lacinia. Integer augue leo, vulputate
-                vitae ligula in, laoreet viverra eros. Proin consequat iaculis
-                pharetra. Fusce ac imperdiet tortor, non scelerisque felis.
-            </span>
+            <MainPage />
         </div>
     );
 }
---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,21 @@
+const MainPage = () => {
+    return (
+        <div>
+            <div className="big-bg-text-container">
+                <span className="big-bg-text">SZYMION</span>
+            </div>
+            <div className="main-container">
+                <div className="text-content">
+                    <h1>Uśmiech to my</h1>
+                    <p>
+                        To jacy jesteśmy, to jak widzą Nas inni, to początek –
+                        od niego wszystko się zaczyna
+                    </p>
+                </div>
+                <div className="image-content"></div>
+            </div>
+        </div>
+    );
+};
+
+export default MainPage;
