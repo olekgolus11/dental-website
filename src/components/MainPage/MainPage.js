7be43eaa68d3e6d523731d27d57bcd13a252b329
olasgolas11 <olekgolus02@gmail.com>
2022-09-02 20:14:51 +0200

    add main screen prototype

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,13 +6,29 @@ const MainPage = () => {
             </div>
             <div className="main-container">
                 <div className="text-content">
-                    <h1>Uśmiech to my</h1>
+                    <div className="line" />
+                    <h1 className="text-usmiech">Uśmiech</h1>
+                    <div className="line" />
+                    <div />
+                    <h1 className="text-tomy">
+                        to <span className="text-my">My</span>
+                    </h1>
+                    <div />
+                    <div />
                     <p>
-                        To jacy jesteśmy, to jak widzą Nas inni, to początek –
-                        od niego wszystko się zaczyna
+                        To jacy jesteśmy, to jak widzą Nas inni,<br></br> to
+                        początek – od niego wszystko się zaczyna
                     </p>
+                    <div />
+                    <div />
+                    <span className="button-poznajoferte">
+                        POZNAJ NASZĄ OFERTĘ
+                    </span>
+                    <div />
                 </div>
-                <div className="image-content"></div>
+                <div className="image-content">
+                    <img className="photo-girl" src="/images/girl-photo.jpg" />
+                </div>
             </div>
         </div>
     );
