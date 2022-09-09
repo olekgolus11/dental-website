e09a437b9c3290117cf40122db8fd72c10e53645
olasgolas11 <olekgolus02@gmail.com>
2022-09-09 17:09:58 +0200

    delete background text (needs fix), init aboutus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
 import MainPage from "./components/MainPage/MainPage";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
     return (
         <div>
             <NavBar />
             <MainPage />
+            <AboutUs />
         </div>
     );
 }
---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+    return (
+        <div className="aboutus-container">
+            <h2 className="title">POZNAJ NAS</h2>
+        </div>
+    );
+};
+
+export default AboutUs;
