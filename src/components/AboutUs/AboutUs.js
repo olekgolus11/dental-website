30359ef840603853a5adce551cf9867f9fadae35
olasgolas11 <olekgolus02@gmail.com>
2022-09-10 21:55:16 +0200

    fix aboutus cards

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,19 +3,22 @@ import Card from "./Card";
 const AboutUs = () => {
     const descriptionsData = [
         {
-            name: "Monika Szymion - stomatolog",
+            name: "Monika Szymion",
+            profession: "Stomatolog",
             description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus mauris ut sem vehicula, eget elementum sapien dignissim. Duis rutrum fringilla orci eget tincidunt. Curabitur congue magna non tortor fringilla lacinia. Integer augue leo, vulputate vitae ligula in, laoreet viverra eros. Proin consequat iaculis pharetra. Fusce ac imperdiet tortor, non scelerisque felis.",
             picture: "/images/dentist.jpg",
         },
         {
-            name: "Rafał Szymion - protetyk",
+            name: "Rafał Szymion",
+            profession: "Protetyk",
             description:
                 "Proin et lacus eros. Vivamus sed quam eu turpis pellentesque consectetur molestie eget tellus. Nulla aliquet eleifend turpis. Suspendisse potenti. Nullam tortor massa, vulputate vitae lectus id, ullamcorper ultricies libero. Pellentesque rutrum, augue ac euismod vehicula, odio orci ultricies augue, in auctor mi quam ut leo.",
             picture: "/images/dentist2.jpg",
         },
         {
-            name: "Karolina Kucharzewska - higienistka",
+            name: "Karolina Kucharzewska",
+            profession: "Higienistka",
             description:
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus mauris ut sem vehicula, eget elementum sapien dignissim. Duis rutrum fringilla orci eget tincidunt. Curabitur congue magna non tortor fringilla lacinia. Integer augue leo, vulputate vitae ligula in, laoreet viverra eros. Proin consequat iaculis pharetra. Fusce ac imperdiet tortor, non scelerisque felis.",
             picture: "/images/dentist3.jpg",
@@ -32,6 +35,7 @@ const AboutUs = () => {
                             key={person.name}
                             picture={person.picture}
                             name={person.name}
+                            profession={person.profession}
                             description={person.description}
                         />
                     ))}
--- src/components/AboutUs/Card.js
+++ src/components/AboutUs/Card.js
@@ -3,6 +3,7 @@ const Card = (props) => {
         <div className="card-container">
             <img src={`${props.picture}`} />
             <h3>{props.name}</h3>
+            <h4>{props.profession}</h4>
             <h6>{props.description}</h6>
         </div>
     );
